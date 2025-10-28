6bd37ad99ecd3ffeddd808228f1b4437590e30b8
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-10-28 14:04:11 -0700

    Add weatherCard and btn

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Buttons() {
-  return <div></div>;
+  return (
+    <div className="btn-container">
+      <button className="btn">Seattle</button>
+      <button className="btn">New York</button>
+      <button className="btn">Paris</button>
+      <button className="btn">London</button>
+      <button className="btn">Moscow</button>
+      <button className="btn">Hong Kong</button>
+    </div>
+  );
 }
 
 export default Buttons;
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,9 +1,14 @@
-import React from "react";
 import InfoCard from "./InfoCard";
+import { TiWeatherCloudy } from "react-icons/ti";
 
-function WeatherCard() {
+function WeatherCard({ weatherInfo, buttonInfo }) {
   return (
-    <div>
+    <div className="weather-card">
+      <div className="weather-icon">
+        <TiWeatherCloudy className="icons" />
+      </div>
+      <h3 className="city-time">{new Date(weatherInfo.current.dt * 1000)}</h3>
+      <h1 className="city-name">{buttonInfo.city}</h1>
       <InfoCard />
     </div>
   );
